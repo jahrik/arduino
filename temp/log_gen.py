34e6352ff69bf585d6294e76f070198722fd00ad
Wes Gill <wgill@kickbackpoints.com>
2017-10-19 03:01:29 -0600

    init

---
+++ temp/log_gen.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""
+Input temp and humid logs from arduino
+Output temp.json
+"""
+import time
+import datetime
+import sys
+import json
+import serial
+
+PORT = sys.argv[1]
+SER = serial.Serial(PORT, 9600, timeout=1)
+time.sleep(3)
+for i in range(0, 10):
+    while True:
+        try:
+            DATA = SER.readline()[:-2]  # the last bit gets rid of the new-line chars
+            if DATA:
+                stamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+                data = json.loads(DATA.decode('utf8'))
+                data['date'] = stamp
+                with open('temps.json', 'a') as outfile:
+                    outfile.write(json.dumps(data, indent=4, sort_keys=True))
+                    outfile.write(',\n')
+
+        except serial.SerialException as exc:
+            # There is no new data from serial port
+            print(exc)
+
+        except TypeError as exc:
+            # Disconnect of USB->UART occured
+            print(exc)
+
+        except ValueError as exc:
+            # json errors
+            print(exc)
---
+++ temp/temp.ino
@@ -0,0 +1,28 @@
+//www.elegoo.com
+//2016.12.9
+
+#include <SimpleDHT.h>
+
+// for DHT11, 
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+int pinDHT11 = 2;
+SimpleDHT11 dht11;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  byte temperature = 0;
+  byte humidity = 0;
+  byte data[40] = {0};
+  if (dht11.read(pinDHT11, &temperature, &humidity, data)) {
+    return;
+  }
+  Serial.print("{\"temp\":\""); Serial.print((int)temperature);
+  Serial.print("\",\"humid\":\""); Serial.print((int)humidity);
+  Serial.println("\"}");
+  delay(1000);
+}
