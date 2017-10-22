066d2af8b48ccc33e3b69fc6f0c27792f6569f43
Wes Gill <wgill@kickbackpoints.com>
2017-10-21 19:39:07 -0600

    Cleanup
    Create functions
    init Celsius to Fahrenheit function

--- temp/log_gen.py
+++ temp/log_gen.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 """
 Input temp and humid logs from arduino
+Add timestamp
+Convert Celsius to Fahrenheit
 Output temp.json
 """
 import time
@@ -11,33 +13,61 @@ import serial
 
 PORT = sys.argv[1]
 
-try:
-    SER = serial.Serial(PORT, 9600, timeout=1)
 
-except serial.SerialException as exc:
-    print(exc)
+def get_serial():
+    """
+    Connect to serial port
+    """
+    try:
+        ser = serial.Serial(PORT, 9600, timeout=1)
 
-time.sleep(3)
-for i in range(0, 10):
-    while True:
-        try:
-            DATA = SER.readline()[:-2]  # the last bit gets rid of the new-line chars
-            if DATA:
-                stamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
-                data = json.loads(DATA.decode('utf8'))
-                data['date'] = stamp
-                with open('temps.json', 'a') as outfile:
-                    outfile.write(json.dumps(data, indent=4, sort_keys=True))
-                    outfile.write(',\n')
+    except serial.SerialException as exc:
+        print(exc)
 
-        except serial.SerialException as exc:
-            # There is no new data from serial port
-            print(exc)
+    return ser
 
-        except TypeError as exc:
-            # Disconnect of USB->UART occured
-            print(exc)
 
-        except ValueError as exc:
-            # json errors
-            print(exc)
+def c_to_f(temp_c):
+    """
+    input Celsius
+    output Fahrenheit
+    """
+    temp_f = 9.0/5.0 * temp_c + 32
+    return temp_f
+
+
+def main():
+    """
+    main functions
+    """
+    ser = get_serial()
+    time.sleep(3)
+    for _int in range(0, 10):
+        while True:
+            try:
+                data = ser.readline()[:-2]  # the last bit gets rid of the new-line chars
+                if data:
+                    stamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+                    json_data = json.loads(data.decode('utf8'))
+                    json_data['date'] = stamp
+                    json_data['temp_f'] = c_to_f(json_data['temp'])
+                    with open('temps.json', 'a') as outfile:
+                        outfile.write(json.dumps(json_data, indent=4, sort_keys=True))
+                        outfile.write(',\n')
+
+            except serial.SerialException as exc:
+                # There is no new data from serial port
+                print(exc)
+
+            except TypeError as exc:
+                # Disconnect of USB->UART occured
+                print(exc)
+
+            except ValueError as exc:
+                # json errors
+                print(exc)
+
+
+if __name__ == '__main__':
+
+    main()
