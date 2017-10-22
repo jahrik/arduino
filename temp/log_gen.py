75acc0fc15f0d5a37aa41eb6e7ae2a2e2b3dc6da
Wes Gill <wgill@kickbackpoints.com>
2017-10-22 12:07:27 -0600

    fix fahr

--- temp/log_gen.py
+++ temp/log_gen.py
@@ -32,7 +32,7 @@ def c_to_f(temp_c):
     input Celsius
     output Fahrenheit
     """
-    temp_f = int(9.0)/int(5.0) * int(temp_c) + int(32)
+    temp_f = int(1.8) * int(temp_c) + int(32)
     return temp_f
 
 
